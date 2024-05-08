2815fe9e5f4daae178b34dd5c5aedb6ea97c2e17
htl-leonding <t.stuetz@htl-leonding.ac.at>
2024-05-08 16:33:30 +0200

    build: add dependencies for rxjava 3 and dagger/hilt

--- todo/app/build.gradle.kts
+++ todo/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlinAndroidKsp)
+    alias(libs.plugins.hiltAndroid)
 }
 
 android {
@@ -66,4 +68,11 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // RxJava
+    implementation (libs.rxjava)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 }
